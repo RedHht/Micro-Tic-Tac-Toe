625230d7a14e5c88439bcb8dba5eb55fee0759ec
Thiago Ianuzzi <thiagoianuzzi4@gmail.com>
2023-11-15 00:56:19 -0300

    major fixes

--- boot.py
+++ boot.py
@@ -16,14 +16,14 @@ def connect_wifi():
             pass
     print('IP Actual:', sta_if.ifconfig()[0])
 
-def crear_ap(essid, password, canal, max_clientes):
+def crear_ap(essid, password, max_clientes):
     # crea una función que recibe cuatro parámetros: el nombre de la red, la contraseña, el canal y el número máximo de clientes
     ap_if = network.WLAN(network.AP_IF) # instancia el objeto -ap_if- para controlar la interfaz AP
     ap_if.active(True) # activa la interfaz AP
-    ap_if.config(essid=essid, password=password, channel=canal, max_clients=max_clientes) # configura los parámetros de la red
+    ap_if.config(essid=essid, password=password, max_clients=max_clientes) # configura los parámetros de la red
     return ap_if # devuelve el objeto -ap_if-
 
 
 
-crear_ap("Tic-Tac-Toe", "tecnica32023", 1, 3)
+crear_ap("Tic-Tac-Toe", "tecnica32023", 3)
 webrepl.start()
